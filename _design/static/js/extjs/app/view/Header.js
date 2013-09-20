7ebe48bae598da1497d2abfd22e278336ef4a96b
her0satr <her0satr@yahoo.com>
2013-09-20 06:10:04 -0700

    design

--- _design/static/js/extjs/app/view/Header.js
+++
@@ -1,8 +0,0 @@
-Ext.define('Stiki.view.Header' ,{
-    extend: 'Ext.container.Container',
-    alias : 'widget.headerpanel',
-	
-    initComponent: function() {
-        this.callParent(arguments);
-    }
-});
